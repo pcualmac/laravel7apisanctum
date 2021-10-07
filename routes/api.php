80b3c339c9a999331bcc5d207c617aa4da08e108
Calogero Pumpignano <jfpum.cal@gmail.com>
2021-10-07 09:32:34 +0100

    Update api.php
    Login not required.

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/login', 'AuthController@login')->name('user.login');
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('product', 'ProductController@index')->name('product');
-    Route::post('product/store', 'ProductController@store')->name('product.store');
-    Route::post('product/destroy', 'ProductController@destroy')->name('product.destroy');
-    Route::post('product/update', 'ProductController@update')->name('product.update');
-});
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::post('/login', 'AuthController@login')->name('user.login');
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('product', 'ProductController@index')->name('product');
+//     Route::post('product/store', 'ProductController@store')->name('product.store');
+//     Route::post('product/destroy', 'ProductController@destroy')->name('product.destroy');
+//     Route::post('product/update', 'ProductController@update')->name('product.update');
+// });
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('product', 'ProductController@index')->name('product');
+Route::post('product/store', 'ProductController@store')->name('product.store');
+Route::post('product/destroy', 'ProductController@destroy')->name('product.destroy');
+Route::post('product/update', 'ProductController@update')->name('product.update');
